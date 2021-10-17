b46261a602c30af162ca0e1e778f5b963e2c912d
Diana <16adianay@gmail.com>
2021-10-17 22:50:47 +0300

    delete "homepage" from package

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from "./components/header/Header";
 import Main from "./components/main/Main";
 import Footer from "./components/footer/Footer";
 import { BrowserRouter } from "react-router-dom";
-import data from './mock.json'
 
 function App() {
     const initialState = JSON.parse(window.localStorage.getItem('tasks')) || [];
